f1ae19374c79d9cc3916d79a0ae963bac10f643e
memsbdm <mehmetbdm@outlook.fr>
2025-10-04 22:06:37 +0400

    feat(design-system): add field

---
+++ packages/design-system/src/atoms/field/field.stories.ts
@@ -0,0 +1,48 @@
+import Field from './field.vue';
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+const meta = {
+	component: Field,
+	title: 'Atoms/Field',
+	argTypes: {
+		label: {
+			type: 'string',
+		},
+		errorMessage: {
+			type: 'string',
+		},
+		helpMessage: {
+			type: 'string',
+		},
+	},
+} satisfies Meta<typeof Field>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Base: Story = {};
+
+export const Textarea: Story = {
+	args: {
+		type: 'textarea',
+	},
+};
+
+export const WithLabel: Story = {
+	args: {
+		label: 'Label',
+	},
+};
+
+export const WithErrorMessage: Story = {
+	args: {
+		errorMessage: 'Error message',
+	},
+};
+
+export const WithHelpMessage: Story = {
+	args: {
+		helpMessage: 'Help message',
+	},
+};
---
+++ packages/design-system/src/atoms/field/field.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+	import { FieldRoot, FieldLabel, FieldInput, FieldTextarea, FieldHelperText, FieldErrorText } from '@ark-ui/vue/field';
+	import { computed } from 'vue';
+	import type { FieldInputProps } from '@ark-ui/vue/field';
+
+	const { errorMessage, type = 'text' } = defineProps<{
+		label?: string;
+		type?: FieldInputProps['type'] | 'textarea';
+		errorMessage?: string;
+		helpMessage?: string;
+	}>();
+
+	const model = defineModel<any>();
+
+	const inputComponent = computed(() => (type === 'textarea' ? FieldTextarea : FieldInput));
+	const invalid = computed(() => typeof errorMessage !== 'undefined');
+</script>
+
+<template>
+	<FieldRoot :invalid class="flex flex-col gap-1">
+		<FieldLabel v-if="label">
+			{{ label }}
+		</FieldLabel>
+
+		<Component
+			:is="inputComponent"
+			v-bind="$attrs"
+			v-model="model"
+			:autoresize="true"
+			:type
+			class="px-4 py-2 border-2 border-gray-800 rounded-lg border-solid bg-transparent resize-y placeholder:text-gray-600 disabled:cursor-not-allowed"
+		/>
+
+		<FieldErrorText class="text-sm text-red-500 uppercase">
+			{{ errorMessage }}
+		</FieldErrorText>
+
+		<FieldHelperText v-if="!errorMessage && helpMessage" class="text-sm text-cyan-500">
+			{{ helpMessage }}
+		</FieldHelperText>
+	</FieldRoot>
+</template>
