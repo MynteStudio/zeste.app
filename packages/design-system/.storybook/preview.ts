5f2a48ac9169731f8d3914dc07ab8902643ee31d
memsbdm <mehmetbdm@outlook.fr>
2025-10-04 16:22:37 +0400

    chore(design-system): setup storybook

---
+++ packages/design-system/.storybook/preview.ts
@@ -0,0 +1,10 @@
+import '../src/styles/reset.css';
+import type { Preview } from '@storybook/vue3-vite';
+
+const preview: Preview = {
+	parameters: {
+		layout: 'centered',
+	},
+};
+
+export default preview;
---
+++ packages/design-system/src/atoms/icon/icon.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+	export const IconName = ['close', 'dropdown', 'success'] as const;
+
+	export type IconName = (typeof IconName)[number];
+</script>
+
+<script lang="ts" setup>
+	defineProps<{
+		name: IconName;
+	}>();
+</script>
+
+<template>
+	<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<use :href="`#${name}`" />
+
+		<symbol id="close">
+			<svg stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+				<path d="M18 6 6 18" />
+				<path d="m6 6 12 12" />
+			</svg>
+		</symbol>
+
+		<symbol id="dropdown">
+			<path d="m6 9 6 6 6-6" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+		</symbol>
+
+		<symbol id="success">
+			<path d="M20 6 9 17l-5-5" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+		</symbol>
+	</svg>
+</template>
